f99869e47578ceb236f85063ae29a8b81069455d
moraiskaua <moraiskaua03@gmail.com>
2024-01-17 10:42:25 -0300

    fix: correctometer height

--- src/app/components/Correctometer.tsx
+++ src/app/components/Correctometer.tsx
@@ -40,7 +40,7 @@ const Correctometer = ({ accuracy }: CorrectometerProps) => {
       <p className="text-2xl font-bold uppercase bg-gradient-to-b from-[#C47BFD] via-[#C47BFD] to-[#343234] inline-block text-transparent bg-clip-text">
         Are you close?
       </p>
-      <div className="w-[85%] h-[90%] rounded-[30px] p-12 bg-[#251926] shadow-custom flex items-center">
+      <div className="w-[85%] h-[478px] rounded-[30px] p-12 bg-[#251926] shadow-custom flex items-center">
         <div className="bg-[#534657] h-full w-14 rounded-full p-2 relative">
           <div className="bg-gradient-to-b from-[#00FEC1] via-[#6E49D7] to-[#FB037A] h-full rounded-full" />
           <TbGuitarPickFilled
