d32515e4aea28f69fe8244b8e829281ccd3aacc6
moraiskaua <moraiskaua03@gmail.com>
2024-02-04 23:41:48 -0300

    fix: config headers on next config

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,19 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com', 'lh3.googleusercontent.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, max-age=0, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withNextIntl(nextConfig);
