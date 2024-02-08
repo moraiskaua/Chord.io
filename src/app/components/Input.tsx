c7cf99e0032c87bb1405263e5a0057701cdd39e8
moraiskaua <moraiskaua03@gmail.com>
2024-02-08 20:04:04 -0300

    feat: create show password button

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import { useState } from 'react';
 import { FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form';
+import { IoMdEye, IoMdEyeOff } from 'react-icons/io';
 
 interface InputProps {
   label: string;
@@ -21,6 +23,8 @@ const Input: React.FC<InputProps> = ({
   errors,
   register,
 }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <div>
       <label
@@ -29,18 +33,29 @@ const Input: React.FC<InputProps> = ({
       >
         {label}
       </label>
-      <div className="mt-2">
+      <div
+        className={`flex justify-center items-center mt-2 outline-none form-input w-full rounded-md p-1.5 bg-[#251926] shadow-sm ring-1 ring-inset ring-primary text-white sm:text-sm sm:leading-6 
+          ${errors[id] && 'focus:ring-[#FB037A]'} ${
+          disabled && 'opacity-50 cursor-default'
+        }`}
+      >
         <input
           id={id}
-          type={type}
+          type={showPassword ? 'text' : type}
           autoComplete={id}
           disabled={disabled}
           {...register(id, { required })}
-          className={`outline-none form-input block w-full rounded-md p-1.5 bg-[#251926] shadow-sm ring-1 ring-inset ring-primary text-white sm:text-sm sm:leading-6 
-          ${errors[id] && 'focus:ring-[#FB037A]'} ${
-            disabled && 'opacity-50 cursor-default'
-          }`}
+          className={`w-full bg-transparent outline-none`}
         />
+        {type === 'password' && (
+          <div onClick={() => setShowPassword(!showPassword)}>
+            {showPassword ? (
+              <IoMdEyeOff className="text-2xl text-primary cursor-pointer" />
+            ) : (
+              <IoMdEye className="text-2xl text-primary cursor-pointer" />
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
